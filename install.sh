a9bd64c044f17035fd9f9b01cc3e9f3341505f43
Florent Cailhol <florent.cailhol@gmail.com>
2013-06-17 11:07:22 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ ! -e "$target" ]; then
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+      echo "Creating $target"
+      ln -s "$PWD/$name" "$target"
+    fi
+  fi
+done
