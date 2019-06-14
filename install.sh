1c2721a650f0fa4626142baa817e3fdd85872591
Florent Cailhol <florent.cailhol@gmail.com>
2019-06-14 15:36:07 +0200

    Simplify install script

--- install.sh
+++ install.sh
@@ -2,21 +2,9 @@
 
 DIR=$(pwd)
 
-_link() {
-  local source=$1
-  local target=$2
-  if [ ! -f $target ]; then
-    mkdir -p "$(dirname $target)"
-    ln -s $source $target
-  fi
-}
-
+# Install dotfiles
+ln -sf "$DIR/.aliases" "$HOME/.aliases"
 ln -sf "$DIR/.editorconfig" "$HOME/.editorconfig"
 ln -sf "$DIR/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$DIR/.gitignore" "$HOME/.gitignore"
 ln -sf "$DIR/.zshrc" "$HOME/.zshrc"
-
-# brew bundle install
-
-# echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
-# chsh -s /usr/local/bin/zsh
